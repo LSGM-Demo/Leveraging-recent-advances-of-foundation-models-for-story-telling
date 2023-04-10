4530233921674315e10403ddc2a6ae3fc936dff5
Tina <78636627+SUcy6@users.noreply.github.com>
2023-04-10 17:25:32 +0800

    4/10 update

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/banner.jpg'); /* banner bg img */
 		
 		// Fixed header
 		//-----------------------------------------------
@@ -28,6 +28,24 @@
 					$("body").removeClass("fixed-header-on");
 				}
 			};
+		});
+
+		var $window = $(window);
+		var $videoWrap = $('.video-wrap');
+		var $video = $('.video');
+		var videoHeight = $video.outerHeight();
+
+		$window.on('scroll',  function() {
+			var windowScrollTop = $window.scrollTop();
+			var videoBottom = videoHeight + $videoWrap.offset().top;
+			
+			if (windowScrollTop > videoBottom) {
+				$videoWrap.height(videoHeight);
+				$video.addClass('stuck');
+			} else {
+				$videoWrap.height('auto');
+				$video.removeClass('stuck');
+			}
 		});
 		
 	   $('#quote-carousel').carousel({
