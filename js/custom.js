a328918b0a6dd0e308a346ced11fa3830baedad7
Tina <78636627+SUcy6@users.noreply.github.com>
2023-04-12 10:43:29 +0800

    4/11

--- js/custom.js
+++ js/custom.js
@@ -31,23 +31,23 @@
 			};
 		});
 
-		var $window = $(window);
-		var $videoWrap = $('.video-wrap');
-		var $video = $('.video');
-		var videoHeight = $video.outerHeight();
+		// var $window = $(window);
+		// var $videoWrap = $('.video-wrap');
+		// var $video = $('.video');
+		// var videoHeight = $video.outerHeight();
 
-		$window.on('scroll',  function() {
-			var windowScrollTop = $window.scrollTop();
-			var videoBottom = videoHeight + $videoWrap.offset().top;
+		// $window.on('scroll',  function() {
+		// 	var windowScrollTop = $window.scrollTop();
+		// 	var videoBottom = videoHeight + $videoWrap.offset().top;
 			
-			if (windowScrollTop > videoBottom) {
-				$videoWrap.height(videoHeight);
-				$video.addClass('stuck');
-			} else {
-				$videoWrap.height('auto');
-				$video.removeClass('stuck');
-			}
-		});
+		// 	if (windowScrollTop > videoBottom) {
+		// 		$videoWrap.height(videoHeight);
+		// 		$video.addClass('stuck');
+		// 	} else {
+		// 		$videoWrap.height('auto');
+		// 		$video.removeClass('stuck');
+		// 	}
+		// });
 		
 	   $('#quote-carousel').carousel({
 		 pause: true,
