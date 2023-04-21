af6495b3c624fbddac3d6dc105bfd300868ce419
Tina <78636627+SUcy6@users.noreply.github.com>
2023-04-21 10:56:59 +0800

    P2

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
 	
 		// $(".banner-image").backstretch('images/banner.jpg'); /* banner bg img */
 		$(".banner-image").backstretch('images/HomePageBGImage.png'); /* banner bg img */
-		$(".promo-image").backstretch('images/P2-IMG2.png');
+		$(".promo-image").backstretch('images/P2-IMG3.png');
 		
 		// Fixed header
 		//-----------------------------------------------
